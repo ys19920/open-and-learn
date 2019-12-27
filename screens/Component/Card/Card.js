a07dadc6bb56555ea53a95f790c21d317e8ed429
yukisanada <yukisanada1992@gmail.com>
2019-12-27 12:25:53 +1000

    card update

--- screens/Component/Card/Card.js
+++ screens/Component/Card/Card.js
@@ -47,7 +47,7 @@ const templateData = [
 
 export default class Card extends React.Component {
   state = {
-    data: templateData[5],
+    data: templateData[4],
     interval: null
   };
 
@@ -133,6 +133,11 @@ export default class Card extends React.Component {
     let status = ['See', 'Watch', 'Link', 'Vote', 'Rate', 'Open'];
     let first_word = this.checktype();
     let other = status.indexOf(first_word);
+    if (first_word == 'See' && !data.image) {
+      <View style={cardStyle.container}>
+        <Text style={{ ...mainStyle.whiteFont, fontSize: 30 }}>{data.description}</Text>
+      </View>;
+    }
     return (
       <View style={cardStyle.container}>
         {other === -1 && (
@@ -162,18 +167,17 @@ export default class Card extends React.Component {
         )}
         {other !== -1 ? (
           <View style={cardStyle.footer}>
-            {first_word !== 'See' ? (
+            {first_word !== 'See' && (
               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Text style={{ ...mainStyle.whiteFont, ...mainStyle.defaultFontSize, flex: 1 }}>
                   {data.title}
                 </Text>
                 {this.showIcon()}
               </View>
-            ) : (
+            )}
+            {first_word === 'See' && data.image && (
               <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                {data.img && (
-                  <Text style={{ color: 'white', fontSize: 20 }}>{data.description}</Text>
-                )}
+                <Text style={{ color: 'white', fontSize: 20 }}>{data.description}</Text>
               </View>
             )}
             {this.showFooter()}
@@ -194,7 +198,7 @@ const cardStyle = StyleSheet.create({
   container: {
     padding: 20,
     width: '100%',
-    height: '70%'
+    height: '100%'
   },
   imageContainer: {
     height: '70%',
@@ -245,6 +249,6 @@ const SeeStyle = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
-    ...cardStyle.container
+    height: '70%'
   }
 });
